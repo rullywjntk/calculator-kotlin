81586e411d85706d2e7c03664a46a1278a691d5c
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-10-12 21:59:35 +0700

    ui-main-layout

--- app/src/main/java/com/rully/calculator/MainActivity.kt
+++ app/src/main/java/com/rully/calculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rully.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.rully.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
     }
 }
